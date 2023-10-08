a1f9478dc2aa7fc00a6d5eb59982b2bd2a10745c
Helen85236 <79532870251@yandex.ru>
2023-10-08 12:42:57 +0300

    Realise previously created task

--- config/settings.py
+++ config/settings.py
@@ -154,10 +154,10 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
-
 # Broker settings for Celery
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_IMPORTS = ("config.tasks", )
 
 # Telegram API settings
 TELEGRAM_API_KEY = os.getenv('TELEGRAM_API_KEY')
